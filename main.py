c4d63d71873e20fc410fcce6b94b7ec577289731
tokizuoh <k_sakai@yumemi.co.jp>
2021-06-14 23:36:10 +0900

    add processing convert kanji or hiragana ... to roman_letters

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import sys
+import pykakasi
+
+
+class Word:
+    def __init__(self, original: str, roman_letters: str):
+        self.original = original
+        self.roman_letters = roman_letters
+
+
+class InvalidArgsError(Exception):
+    pass
+
+
+def convertRomanLetters(sentence: str) -> [Word]:
+    kks = pykakasi.kakasi()
+    res = kks.convert(sentence)
+    words = []
+    for item in res:
+        words.append(Word(original=item['orig'], roman_letters=item['hepburn']))
+    return words
+
+
 def main():
-    print("Hello, World!")
+    args = sys.argv
+    if len(args) == 1:
+        raise InvalidArgsError
+    
+    target = args[1]
+    words = convertRomanLetters(sentence=target)
+    
+    for w in words:
+        print(w.original, w.roman_letters)
+
 
 if __name__ == "__main__":
     main()
