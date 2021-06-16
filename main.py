6195a81c6dd7240621a486244e75136483ee097b
tokizuoh <k_sakai@yumemi.co.jp>
2021-06-16 21:03:09 +0900

    add processing extract "word" from DB

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 import sys
 import pykakasi
+import sqlite3
 
 
 class Word:
-    def __init__(self, original: str, roman_letters: str):
-        self.original = original
-        self.roman_letters = roman_letters
+    def __init__(self, word_id: int, lemma: str, roman: str):
+        self.word_id = word_id
+        self.lemma = lemma
+        self.roman = roman
 
 
 class InvalidArgsError(Exception):
     pass
 
 
-def convertRomanLetters(sentence: str) -> [Word]:
-    kks = pykakasi.kakasi()
-    res = kks.convert(sentence)
-    words = []
-    for item in res:
-        words.append(Word(original=item['orig'], roman_letters=item['hepburn']))
-    return words
+# def convertRomanLetters(sentence: str) -> [Word]:
+#     kks = pykakasi.kakasi()
+#     res = kks.convert(sentence)
+#     words = []
+#     for item in res:
+#         words.append(Word(original=item['orig'], roman_letters=item['hepburn']))
+#     return words
+
 
 
 def main():
-    args = sys.argv
-    if len(args) == 1:
-        raise InvalidArgsError
+    con = sqlite3.connect('wnjpn.db')
+    cur = con.cursor()
+    cur.execute("select * from word where lang='jpn'")
+    for r in cur:
+        print(r)
+
+    # args = sys.argv
+    # if len(args) == 1:
+    #     raise InvalidArgsError
     
-    target = args[1]
-    words = convertRomanLetters(sentence=target)
+    # target = args[1]
+    # words = convertRomanLetters(sentence=target)
     
-    for w in words:
-        print(w.original, w.roman_letters)
+    # for w in words:
+    #     print(w.original, w.roman_letters)
 
 
 if __name__ == "__main__":
