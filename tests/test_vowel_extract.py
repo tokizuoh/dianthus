b0cdb0e37684949f200e346e26da82d052cd3fe9
tokizuoh <k_sakai@yumemi.co.jp>
2021-06-18 22:43:18 +0900

    add unittest (extract vowel)

--- tests/test_vowel_extract.py
+++ tests/test_vowel_extract.py
@@ -4,10 +4,52 @@ from vowel import vowel
 
 
 class TestExtractVowel(unittest.TestCase):
-    def test_extract_vowel(self):
+    def test_extract_vowel_normal(self):
         value = 'amana'
 
         expected = 'aaa'
+        actual = vowel.extract(value)
+        self.assertEqual(expected, actual)
+
+    def test_extract_vowel_include_chi(self):
+        value = 'chiyuki'
+
+        expected = 'iui'
+        actual = vowel.extract(value)
+        self.assertEqual(expected, actual)
+
+    def test_extract_vowel_continuous_vowel(self):
+        value = 'oosaki'
+
+        expected = 'ooai'
+        actual = vowel.extract(value)
+        self.assertEqual(expected, actual)
+
+    def test_extract_vowel_continuous_vowel(self):
+        value = 'oosaki'
+
+        expected = 'ooai'
+        actual = vowel.extract(value)
+        self.assertEqual(expected, actual)
+    
+    def test_extract_vowel_include_jyo(self):
+        value = 'saijyo'
+
+        expected = 'aio'
+        actual = vowel.extract(value)
+        self.assertEqual(expected, actual)
+
+    def test_extract_vowel_only_vowel(self):
+        value = 'aaa'
+
+        expected = 'aaa'
+        actual = vowel.extract(value)
+        self.assertEqual(expected, actual)
+
+    def test_extract_vowel_include_na(self):
+        value = 'natsuha'
+
+        expected = 'aua'
         actual = vowel.extract(value)
         self.assertEqual(expected, actual)
 
